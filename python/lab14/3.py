f938ffd1f6789d27e47fcb62a0f338125f635c2d
bose-ankita <ankitabose509@gmail.com>
2025-09-04 17:09:04 +0530

    Add files via upload

---
+++ python/lab14/3.py
@@ -0,0 +1,30 @@
+#Input two integers k,n
+#Output arithmetic operations between k and n
+
+k=1
+n=10
+summ=k+n
+diff=k-n
+product=k*n
+div=k/n
+expo=k**n
+intdiv=k//n
+mod=k%n
+
+print(summ)
+print(diff)
+print(product)
+print(div)
+print(expo)
+print(intdiv)
+print(mod)
+
+#output:
+#11
+#-9
+#10
+#0.1
+#1
+#0
+#1
+
---
+++ python/lab14/4.py
@@ -0,0 +1,18 @@
+#Quardratic equation
+
+import cmath
+a=1
+b=10
+c=7
+
+d=b*b - 4*a*c
+
+print(d)
+
+root1=((-b + cmath.sqrt(d))/2*a)
+root2=((-b - cmath.sqrt(d))/2*a)
+
+print("root 1 is", root1)
+print("root 2 is", root2)
+
+
